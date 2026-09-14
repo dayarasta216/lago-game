87dd2a8cf963f579e5687d52ef1715e1a2560fa4
dayarasta216 <mariyafilimonova086@gmail.com>
2026-09-15 00:05:54 +0700

    Update lago-games.js

--- lago-games.js
+++ lago-games.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = 10;
+  const VERSION = 11;
 
 let overlay = null;
 
@@ -37,7 +37,7 @@ const GAME_MODULES =
         load() {
 
           return import(
-  "./lago-game-knife.js?v=12"
+  "./lago-game-knife.js?v=13"
 );
         }
 
