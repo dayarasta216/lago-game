d7fdeb9351160fe6690813e595c4f3cc48aac8c2
dayarasta216 <mariyafilimonova086@gmail.com>
2026-09-21 10:49:20 +0700

    Update lago-games.js

--- lago-games.js
+++ lago-games.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = 37;
+  const VERSION = 38;
 
 let overlay = null;
 
@@ -37,7 +37,7 @@ const GAME_MODULES =
         load() {
 
           return import(
-            "./lago-game-tanks.js?v=18"
+            "./lago-game-tanks.js?v=19"
           );
 
         }
